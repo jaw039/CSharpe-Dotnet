f349fc8851782f38574c07e9d41ccab45eac4ffd
jaw039(UCSD) <150192423+jaw039@users.noreply.github.com>
2025-02-20 14:21:07 -0800

    method that returns an array

---
+++ TestProject.MethodReturnArray/Program.cs
@@ -0,0 +1,42 @@
+﻿int target = 80;
+int[] coins = new int[] {5, 5, 50, 25, 25, 10, 5};
+int[,] result = TwoCoins(coins, target);
+
+if (result.Length == 0) 
+{
+    Console.WriteLine("No two coins make change");
+} 
+else 
+{
+    Console.WriteLine("Change found at positions:");
+    for (int i = 0; i < result.GetLength(0); i++) 
+    {
+        if (result[i,0] == -1) 
+        {
+            break;
+        }
+        Console.WriteLine($"{result[i,0]},{result[i,1]}");
+    }
+}
+
+// Returns multiple pairs of indices as int[,]
+int[,] TwoCoins(int[] coins, int target) 
+{
+    int[,] result = {{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1}}; // Can store up to 5 pairs
+    int count = 0;
+
+    // Finds all possible pairs that sum to target
+    for (int curr = 0; curr < coins.Length; curr++) 
+    {
+        for (int next = curr + 1; next < coins.Length; next++) 
+        {
+            if (coins[curr] + coins[next] == target) 
+            {
+                result[count, 0] = curr;
+                result[count, 1] = next;
+                count++;
+            }
+        }
+    }
+    return (count == 0) ? new int[0,0] : result;
+}
