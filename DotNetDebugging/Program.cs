fb6662a41159018a9d7859c9253cd1a65fc61d0d
jaw039 <jaw039@ucsd.edu>
2025-09-01 14:13:34 -0700

    Add debug logging to Fibonacci method
    Introduced Debug.WriteLine statements in the Fibonacci function to log method entry, target number, and each iteration's state. This will help trace the computation and facilitate debugging.

--- DotNetDebugging/Program.cs
+++ DotNetDebugging/Program.cs
@@ -2,17 +2,22 @@
 
 int result = Fibonacci(5);
 Console.WriteLine(result);
+
 static int Fibonacci(int n)
 {
     int n1 = 0;
     int n2 = 1;
     int sum;
+    Debug.WriteLine($"Entering {nameof(Fibonacci)} method");
+    Debug.WriteLine($"We are looking for the {n}th number");
 
     for (int i = 2; i <= n; i++)
     {
         sum = n1 + n2;
         n1 = n2;
         n2 = sum;
+        Debug.WriteLine($"Iteration {i}: n1 = {n1}, n2 = {n2}, sum = {sum}");
+        Debug.WriteLineIf(sum == 1, $"sum is 1, n1 is {n1}, n2 is {n2}");
     }
 
     return n == 0 ? n1 : n2;
--- DotNetDebugging/obj/Debug/net9.0/DotNetDebugging.AssemblyInfo.cs
+++ DotNetDebugging/obj/Debug/net9.0/DotNetDebugging.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetDebugging")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2afb788ef72bd92e1a7d29941b2b7b20c1b4f881")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ee762e40fb3aad820d70b8612164043fcaf2ca3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetDebugging")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetDebugging")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
